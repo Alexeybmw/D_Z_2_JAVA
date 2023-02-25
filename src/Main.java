3e8dd2aa8a03403681e7eae168b0d216da52dae4
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-02-25 13:21:33 +0300

    revision 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticet = 13676;
+        int ticket = 13676;
         int bonus = 20;
 
-        System.out.println("Бонусные мили: " + ticet / bonus);
+        System.out.println("Бонусные мили: " + ticket / bonus);
     }
 }
