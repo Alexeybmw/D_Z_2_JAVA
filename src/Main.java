c53bdea31a60223e174d7194f5e9c7045da269fd
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-02-25 12:03:58 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticet;
+        int bonus;
+        ticet = 13676;
+        bonus = 20;
+        System.out.println("Бонусные мили: " + ticet / bonus);
+    }
+}
