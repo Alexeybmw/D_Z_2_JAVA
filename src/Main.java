52aadbcbc1a939e7693fa0c4a35ccb97285989c1
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-02-25 14:35:57 +0300

    formatting

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
         int ticket = 13676;
         int bonus = 20;
-
         System.out.println("Бонусные мили: " + ticket / bonus);
     }
 }
